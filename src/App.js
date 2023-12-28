a6ff7a3b68b36875ab69ab514dd00554c890985e
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-28 15:23:20 -0600

    title size fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./styles/App.css";
 function App() {
   return (
     <div className="container">
-      <Header title="PASSWORD GENERATOR" subtitle="create strong and secure password"/>
+      <Header
+        title="PASSWORD GENERATOR"
+        subtitle="Create strong and secure passwords to keep your account safe online."
+      />
     </div>
   );
 }
