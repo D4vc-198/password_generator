234615f62fe1fa98a93831d64fd41821805d76c5
d4vc-198 <contactodiegoavc@gmail.com>
2023-12-28 15:05:11 -0600

    header text added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Header from "./components/header";
 import "./styles/App.css";
 
 function App() {
   return (
-    <div className="container"></div>
+    <div className="container">
+      <Header title="PASSWORD GENERATOR" subtitle="create strong and secure password"/>
+    </div>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,12 @@
-const Header = () => {
+const Header = (props) => {
+    const {title, subtitle} = props
     
+    return (
+        <>
+            <h1>{title}</h1>
+            <h3>{subtitle}</h3>
+        </>
+    )
 }
 
 export default Header
