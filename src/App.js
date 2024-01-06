23f99209b378f003c045b4a1484cc237c9211dab
d4vc-198 <contactodiegoavc@gmail.com>
2024-01-05 21:36:57 -0600

    password config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import PasswordTxtContainer from "./components/PasswordTxtContainer";
 import Header from "./components/header";
 import PasswordConfig from "./components/passwordConfig";
 import "./styles/App.css";
 
 function App() {
+  const [password, setPassword] = useState("");
+
   return (
     <div className="container">
       <Header
--- src/components/passwordConfig.js
+++ src/components/passwordConfig.js
@@ -1,35 +1,77 @@
+import { useEffect, useState } from "react";
 import "./../styles/passwordConfig.css";
 
 const PasswordConfig = () => {
+  const [passLength, setPassLength] = useState(8);
+  const [checkboxOptions, setCheckboxOptions] = useState({
+    uppercase: false,
+    lowercase: false,
+    numbers: false,
+    specialChar: false,
+  });
+
+  const handleCheckboxOption = (option) => {
+    setCheckboxOptions((prevState) => ({
+      ...prevState,
+      [option]: !prevState[option],
+    }));
+  };
+
+  useEffect(() => {
+    console.log(checkboxOptions);
+  }, [checkboxOptions]);
+
   return (
     <div className="password-config-container">
       {/* PASSWORD Length */}
       <div className="password-length-container">
-        <p>Password Length: 0</p>
+        <p>Password Length: {passLength}</p>
         <input
           className="password-select-length"
           type="range"
           min="0"
           max="100"
+          value={passLength}
+          onChange={(e) => setPassLength(e.target.value)}
         />
       </div>
       {/* PASSWORD CHARACTERS CONTAINER */}
       <div className="checkbox-container">
         <div className="input-checkbox-container">
           <label>Uppercase</label>
-          <input type="checkbox" className="checkbox"/>
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={checkboxOptions.uppercase}
+            onChange={() => handleCheckboxOption("uppercase")}
+          />
         </div>
         <div className="input-checkbox-container">
           <label>Lowercase</label>
-          <input type="checkbox" className="checkbox" />
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={checkboxOptions.lowercase}
+            onChange={() => handleCheckboxOption("lowercase")}
+          />
         </div>
         <div className="input-checkbox-container">
           <label>Numbers</label>
-          <input type="checkbox" className="checkbox" />
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={checkboxOptions.numbers}
+            onChange={() => handleCheckboxOption("numbers")}
+          />
         </div>
         <div className="input-checkbox-container">
           <label>Special Characters</label>
-          <input type="checkbox" className="checkbox" />
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={checkboxOptions.specialChar}
+            onChange={() => handleCheckboxOption("specialChar")}
+          />
         </div>
       </div>
       <div className="btn-generate-password-container">
