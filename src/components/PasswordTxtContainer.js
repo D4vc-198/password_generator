27763316bf2979380f57e603850573688c3f3cf2
d4vc-198 <contactodiegoavc@gmail.com>
2024-01-08 22:50:03 -0600

    function txt copy added

--- src/components/PasswordTxtContainer.js
+++ src/components/PasswordTxtContainer.js
@@ -1,13 +1,19 @@
 import { IoReload, IoClipboard } from "react-icons/io5";
 
 import "./../styles/PasswordTxtContainer.css";
+import { useState } from "react";
 
 const PasswordTxtContainer = (props) => {
   const { password } = props;
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(password)
+  }
+
   return (
     <div className="password-container">
       <input type="text" className="password-input" value={password} readOnly />
-      <button className="btn-password-copy">
+      <button className="btn-password-copy" onClick={copy} disabled={!password}>
         <IoClipboard />
         &nbsp;Copy
       </button>
